6f7b1e324d95c4db5e16fc33f550280198a82cc1
荒井美香 <>
2024-01-27 20:17:23 +0900

    075_Fragment

---
+++ react-guide-material/04_react_basic/src/070_component_nest/start/components/Child.js
@@ -0,0 +1,14 @@
+import "./Child.css";
+
+const Child = () => {
+  return (
+    <div className="component">
+      <h3>練習問題</h3>
+      <div className="components">
+        <h3>Child Components</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Child;
