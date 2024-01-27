7287913e1d9c2f1dd63c38ab67cee92dcd85452c
荒井美香 <>
2024-01-27 21:10:40 +0900

    080_jsx

---
+++ react-guide-material/04_react_basic/src/080_expr_in_jsx/start/components/Expression.js
@@ -0,0 +1,11 @@
+import "./Expression.css";
+
+const Expression = () => {
+    const title = "Expression";
+    return (
+        <div className="expression">
+            <h3>Hello {title}</h3>
+        </div>
+    )
+}
+export default Expression;
